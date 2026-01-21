199ddb0876d17cdd80a80a8742c5d039578af2e4
AK71103 <A_kazantzis@mail.harpercollege.edu>
2026-01-21 14:18:19 -0600

    Update PS3P5.py
    added code

--- PS3P5.py
+++ PS3P5.py
@@ -1,1 +1,11 @@
-
+#Input phase
+Rad = (input("Enter radius: "))
+#Process phase
+Pi = int(3.14)
+HPrmtr = int(Rad)*int(Pi)
+Prmtr = 2*HPrmtr
+R2 = int(Rad)**int(2)
+Area = (Pi*R2)
+#Output phase
+print(Prmtr)
+print(Area)
